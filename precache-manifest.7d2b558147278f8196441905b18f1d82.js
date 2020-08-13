090eb1b26414cdbe7d8a17f5076c788f36efe122
Bpedley <v3rtex12@gmail.com>
2020-08-13 17:16:09 +0300

    Updates

---
+++ precache-manifest.7d2b558147278f8196441905b18f1d82.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa50ddbb343c4c4fc11253cabb6623be",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "77afa3111f511e6e84d1",
+    "url": "/shopping-cart/static/css/2.2738ee7a.chunk.css"
+  },
+  {
+    "revision": "2f8cbb7ea34d1faa7233",
+    "url": "/shopping-cart/static/css/main.a7e59919.chunk.css"
+  },
+  {
+    "revision": "77afa3111f511e6e84d1",
+    "url": "/shopping-cart/static/js/2.258ce5b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart/static/js/2.258ce5b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f8cbb7ea34d1faa7233",
+    "url": "/shopping-cart/static/js/main.c34ecdbd.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shopping-cart/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shopping-cart/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shopping-cart/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shopping-cart/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shopping-cart/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
